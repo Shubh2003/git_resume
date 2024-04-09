3bad5d081c1af5937268e37e680c68031a6daf88
Shubham Daulkar <daulkarshubham@gmail.com>
2024-04-09 14:03:53 +0530

    added repository component

---
+++ components/Repositories.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+interface GithubRepo{
+    id:number;
+    name:string;
+    html_url:string;
+    language:string;
+    homepage?:string;
+    watchers:number;
+    forks:number;
+    watchersLabel:string;
+    forksLabel:string;
+    date:number;
+}
+
+interface RepositoriesProps{
+    repos:GithubRepo[];
+    repoCount:number;
+}
+
+const Repositories: React.FC<RepositoriesProps> = ({repos,repoCount}) => {
+    return (
+        <div className="mt-5 px-2 text-[#F8FAFC]">
+            <h2 className="text-2xl mb-4 font-bold underline text-left">
+        Popular Repositories
+            </h2>
+            <ul className="list-disc px-4">
+                {repos.slice(0,repoCount).map((repo)=>(
+                     <li key={repo.id} className="mt-4">
+                        <div className="flex flex-col space-y-2">
+                            <div className="flex items-center justify-between">
+                                <a 
+                                href={repo.html_url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-lg font-semibold text-blue-500 underline"
+                                >
+                                    {repo.name}
+                                </a>
+                                <span className="text-sm">({repo.date})</span>
+                            </div>
+                            {repo.language && (
+                            <>
+                                <a 
+                                   href={repo.homepage}
+                                   target="_blank"
+                                   rel="noopener noreferrer"
+                                   className="text-primary hover:underline pb-1"
+                                >
+                                    {repo.homepage}
+                                </a>
+                            </>
+                            )}
+                            <p className="text-accent-500">
+                                <span className="font-semibold">{repo.language}</span>
+                                &nbsp;-&nbsp;
+                                {"Owner"}
+                            </p>
+                            <p className="text-gray-400 text-sm">
+                                This repository has&nbsp;
+                                <span className="font-semibold">{repo.watchers}</span>
+                                {repo.watchersLabel} ans&nbsp;
+                                <span className="font-semibold">{repo.forks}</span>
+                                {repo.forksLabel}. If you would like more information about this
+                                repository and my contributed code, please visit the
+                                <a href={repo.html_url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-blue-500 hover:underline ml-1"
+                                >
+                                    repository&nbsp;
+                                </a>
+                                on Github.
+                            </p>
+                        </div>
+                     </li>
+                ))}
+               
+            </ul>
+        </div>
+    )
+}
+
+export default Repositories;
